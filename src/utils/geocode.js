36f342b029bc2ddc65c71010614daa280aaa1809
Atul Jain <connectme.atul@gmail.com>
2020-12-11 23:38:24 -0600

    fix crashing issue

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -10,7 +10,7 @@ const geocode = (address, callback) => {
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
-        } else if (body.features.length === 0) {
+        } else if (body || body.features.length === 0) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
             callback(undefined, {
